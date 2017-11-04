5830448de0ce74bbd2be1749ae11665bf3a5e27a
Nicole Sattler <nicole.sattler@live.com>
2017-11-04 22:37:51 +0000

    Added ScrollReveal config

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 /* global $ */
 /* global projects */
+/* global ScrollReveal */
+/* global sr */
+
 
 $(document).ready(function() {
+
 
    //On window load/resize show/hide mobile nav
    $(window).resize(function() {
@@ -18,10 +22,10 @@ $(document).ready(function() {
    $(".fa-bars").on("click", function() {
       var scrollTop = $(window).scrollTop(),
          height = $(window).height();
-      
+
       $("nav ul").slideToggle();
-      if(scrollTop < height - 45){
-         $("nav").css({"background": "rgba(0,0,0,0.8)"});
+      if (scrollTop < height - 45) {
+         $("nav").css({ "background": "rgba(0,0,0,0.8)" });
       }
    });
 
@@ -36,11 +40,11 @@ $(document).ready(function() {
          height = $(window).height();
 
       if (scrollTop > height - 45) {
-         $("nav").css({"background": "#fff", "box-shadow": "0 1px 3px rgba(0,0,0,0.1)"});
+         $("nav").css({ "background": "#fff", "box-shadow": "0 1px 3px rgba(0,0,0,0.1)" });
          $("nav li, nav .fa-bars").css("color", "#666");
       }
       else {
-         $("nav").css({"background": "none", "box-shadow": "none"});
+         $("nav").css({ "background": "none", "box-shadow": "none" });
          $("nav li, nav .fa-bars").css("color", "#ccc");
       }
    });
@@ -80,13 +84,13 @@ $(document).ready(function() {
 
    //Close Popup Info
    $(document).on("click", function(e) {
-      
-      if(!$(e.target).closest("#popupInfo").length && !$(e.target).is(".btn")){
+
+      if (!$(e.target).closest("#popupInfo").length && !$(e.target).is(".btn")) {
          $("#popupInfo").hide("fade");
       }
    });
-   
-   $(".close").on('click', function(){
+
+   $(".close").on('click', function() {
       $("#popupInfo").hide("fade");
    });
 
@@ -99,18 +103,54 @@ $(document).ready(function() {
    });
 
    //Populate projects section
-   projects.forEach(function(project){
-      
-      var projectDiv = '<div class="project"><img src='+ project.img +'><span class="projectDetails"><h5>' + project.name + '</h5>' + project.description + '<a href="' + project.link +'" target="_blank "><button>View Live</button></a></span></div>';
-      
+   projects.forEach(function(project) {
+
+      var projectDiv = '<div class="project"><img src=' + project.img + '><span class="projectDetails"><h5>' + project.name + '</h5>' + project.description + '<a href="' + project.link + '" target="_blank "><button>View Live</button></a></span></div>';
+
       $('.projects section').append(projectDiv);
    });
-   
+
    //Hide project details
    $(".projectDetails").hide();
-   
+
    /*
 <div class="project "><img src="images/wiki1.png "><span class="projectDetails "><h5>Wikipedia Viewer</h5> Intermediate Front-End Project<br>HTML, CSS & jQuery<a href="https://codepen.io/nikkilr88/full/PpdBEe" target="_blank "><button>View Live</button></a></span></div>
 */
+
+   //ScrollReveal Config
+
+   window.sr = ScrollReveal();
+ 
+   sr.reveal('.txtbg h1', {
+      duration: 1000,
+      origin: 'left',
+      distance: '300px'
+   });
+
+   sr.reveal('.txtbg h2', {
+      duration: 1000,
+      origin: 'right',
+      distance: '300px',
+      delay: 1000
+   });
+   
+   sr.reveal('.featurette img', {
+      duration: 2000,
+      origin: 'left',
+      distance: '300px',
+      viewFactor: 0.2
+   });
+   
+   sr.reveal('.featurette section', {
+      duration: 2000,
+      origin: 'right',
+      distance: '300px',
+      viewFactor: 0.2
+   });
+
+   sr.reveal('.box, .project', {
+      duration: 2000,
+      viewFactor: 0.2
+   });
 
 });
