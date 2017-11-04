c6779203fbbf15acc4f8c3486a6917062ce93c18
Nicole Sattler <nicole.sattler@live.com>
2017-11-04 21:31:41 +0000

    Added js directory

---
+++ js/projects.js
@@ -0,0 +1,56 @@
+var projects = [
+	{
+		name: 'Twitter Bot',
+		img: 'images/twitter-bot.png',
+		description: 'Node.js, npm package "twit"',
+		link: 'https://twitter.com/GitLit000'
+	},
+	{
+		name: 'Giphy API',
+		img: 'images/giphy.png',
+		description: 'HTML, CSS & jQuery',
+		link: 'https://nikkilr88.github.io/giphy'
+	},
+	{
+		name: 'Random Quote Generator V2',
+		img: 'images/quote-react.png',
+		description: 'HTML, CSS & ReactJS',
+		link: 'https://nikkilr88.github.io/fcc-random-quote/'
+	},
+	{
+		name: 'Calculator V2',
+		img: 'images/cal2.png',
+		description: 'HTML, Scss & jQuery',
+		link: 'https://nikkilr88.github.io/fcc-calculatorv2/'
+	},
+	{
+		name: 'Google Homepage',
+		img: 'images/google-home.png',
+		description: 'HTML, Scss & jQuery',
+		link: 'https://nikkilr88.github.io/google-homepage/'
+	},
+	{
+		name: 'Pomodoro',
+		img: 'images/timer.png',
+		description: 'Intermediate Front-End Project<br>HTML, Scss & jQuery',
+		link: 'https://codepen.io/nikkilr88/full/ybgWPb/'
+	},
+	{
+		name: 'Twitch API Basic',
+		img: 'images/twitch-api.png',
+		description: 'Intermediate Front-End Project<br>HTML, Scss & jQuery',
+		link: 'https://codepen.io/nikkilr88/full/mmMBJM'
+	},
+	{
+		name: 'Local Weather',
+		img: 'images/weather.png',
+		description: 'Intermediate Front-End Project<br>HTML, Scss & jQuery',
+		link: 'https://codepen.io/nikkilr88/full/LWdeyY'
+	},
+	{
+		name: 'Wikipedia Viewer',
+		img: 'images/wiki1.png',
+		description: 'Intermediate Front-End Project<br>HTML, Scss & jQuery',
+		link: 'https://codepen.io/nikkilr88/full/PpdBEe/'
+	}
+];
---
+++ js/script.js
@@ -0,0 +1,116 @@
+/* global $ */
+/* global projects */
+
+$(document).ready(function() {
+
+   //On window load/resize show/hide mobile nav
+   $(window).resize(function() {
+      if ($(window).width() < 925) {
+         //Open and close mobile menu
+         $("nav ul").hide();
+      }
+      else {
+         $("nav ul").show();
+      }
+   });
+
+   //Open and close nav
+   $(".fa-bars").on("click", function() {
+      var scrollTop = $(window).scrollTop(),
+         height = $(window).height();
+      
+      $("nav ul").slideToggle();
+      if(scrollTop < height - 45){
+         $("nav").css({"background": "rgba(0,0,0,0.8)"});
+      }
+   });
+
+
+   //Change menu color on scroll
+   $(window).scroll(function() {
+      if ($(window).width() < 925) {
+         $("nav > ul").hide(500);
+      }
+
+      var scrollTop = $(window).scrollTop(),
+         height = $(window).height();
+
+      if (scrollTop > height - 45) {
+         $("nav").css({"background": "#fff", "box-shadow": "0 1px 3px rgba(0,0,0,0.1)"});
+         $("nav li, nav .fa-bars").css("color", "#666");
+      }
+      else {
+         $("nav").css({"background": "none", "box-shadow": "none"});
+         $("nav li, nav .fa-bars").css("color", "#ccc");
+      }
+   });
+
+   //Scroll to section
+   $('#about').on('click', function() {
+      $('html, body').animate({
+         scrollTop: $('.aboutMe').offset().top
+      }, 1000);
+   });
+
+   $('#projects').on('click', function() {
+      $('html, body').animate({
+         scrollTop: $('.projects').offset().top
+      }, 1000);
+   });
+
+   //Popup Info
+   $("#html").on("click", function() {
+      $('#popupInfo #heading').text("HTML / CSS");
+      $('#popupInfo #content').html("<p>I began Learning the basics of HTML and CSS in January of 2017. I started off with Codecademy and later stumbled upon FreeCodeCamp. I completed all of their frontend lessons and projects, and eventually recieved my frontend certificate in May of 2017.</p><p>I have also completed a Codecademy course for responsive design and began their HTML & CSS course. I am currently putting it off to the side to focus on my bootcamp.<a href=\"images/cert.png\"><img src='images/cert.png' /></a>");
+      $('#popupInfo').show("fade");
+   });
+
+   $("#js").on("click", function() {
+      $('#popupInfo #heading').text("JavaScript");
+      $('#popupInfo #content').html("<p>JavaScript is where the fun begins! I began learning vanilla JS with both Codecademy and FreeCodeCamp and have since completed both courses. FCC's algorithm challenges were very helpful because they taught me how do my own research and code with others.</p><p>I have also completed a couple of ReactJS courses and began building some FCC projects. This has been one of my favorite JS libraries so far.</p> <a href='https://www.freecodecamp.org/nikkilr88' target='_blank'>Have a look at some of the algorithm challenges I have completed</a>");
+      $('#popupInfo').show("fade");
+
+   });
+
+   $("#backend").on("click", function() {
+      $('#popupInfo #heading').text("Backend");
+      $('#popupInfo #content').html("<p>This has been my favorite so far. I began learning Node.js and MongoDB in an online bootcamp, which I am still currently working though. I made a basic blog and movie search app, which taught me about RESTful routing, how to access data from APIs and how to use Mongoose.</p><p>I am excited to continue with the bootcamp and improve on my skills that I have already learned.</p>");
+      $('#popupInfo').show("fade");
+   });
+
+   //Close Popup Info
+   $(document).on("click", function(e) {
+      
+      if(!$(e.target).closest("#popupInfo").length && !$(e.target).is(".btn")){
+         $("#popupInfo").hide("fade");
+      }
+   });
+   
+   $(".close").on('click', function(){
+      $("#popupInfo").hide("fade");
+   });
+
+   //Project section
+   $(".projects").on("mouseover", ".project", function() {
+      $(this).find(".projectDetails").show("fade");
+   });
+   $(".projects").on("mouseleave", ".project", function() {
+      $(this).find(".projectDetails").hide("fade");
+   });
+
+   //Populate projects section
+   projects.forEach(function(project){
+      
+      var projectDiv = '<div class="project"><img src='+ project.img +'><span class="projectDetails"><h5>' + project.name + '</h5>' + project.description + '<a href="' + project.link +'" target="_blank "><button>View Live</button></a></span></div>';
+      
+      $('.projects section').append(projectDiv);
+   });
+   
+   //Hide project details
+   $(".projectDetails").hide();
+   
+   /*
+<div class="project "><img src="images/wiki1.png "><span class="projectDetails "><h5>Wikipedia Viewer</h5> Intermediate Front-End Project<br>HTML, CSS & jQuery<a href="https://codepen.io/nikkilr88/full/PpdBEe" target="_blank "><button>View Live</button></a></span></div>
+*/
+
+});
