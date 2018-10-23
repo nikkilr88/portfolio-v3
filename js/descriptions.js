d8a7dce4036b93703c6e57ce8ee775d7507addba
Nicole <nicole.sattler@live.com>
2018-10-23 12:42:59 -0500

    Fix image paths

--- js/descriptions.js
+++ js/descriptions.js
@@ -1,7 +1,9 @@
 const descriptions = {
-        front: "<p>I began Learning the basics of HTML and CSS in January of 2017. I started off with Codecademy and later stumbled upon FreeCodeCamp. I completed all of their frontend lessons and projects, and eventually recieved my frontend certificate in May of 2017.</p><p>I have also completed a Codecademy course for responsive design and began their HTML & CSS course. I am currently putting it off to the side to focus on my bootcamp.</p><a href=\"images/cert.png\" target=\"_blank\"><img src='images/cert.png' /></a> <a href=\"images/design-cert.jpg\" target=\"_blank\"><img src='images/design-cert.jpg' /></a>",
-   
-        back: "<p>This has been my favorite so far. I began learning Node.js and MongoDB in an online bootcamp building out a fullstack production web application. Afterwards I continued improving my skills building bots and CRUD applications.</p><p>I am currently working on a few side projects such as an e-mail microservice and an API with an admin dashboard with CRUD capabilities.</p>",
-    
-        data: ""
-    };
+  front:
+    '<p>I began Learning the basics of HTML and CSS in January of 2017. I started off with Codecademy and later stumbled upon FreeCodeCamp. I completed all of their frontend lessons and projects, and eventually recieved my frontend certificate in May of 2017.</p><p>I have also completed a Codecademy course for responsive design and began their HTML & CSS course. I am currently putting it off to the side to focus on my bootcamp.</p><a href="images/cert.png" target="_blank"><img src=\'../images/cert.png\' /></a> <a href="images/design-cert.jpg" target="_blank"><img src=\'../images/design-cert.jpg\' /></a>',
+
+  back:
+    '<p>This has been my favorite so far. I began learning Node.js and MongoDB in an online bootcamp building out a fullstack production web application. Afterwards I continued improving my skills building bots and CRUD applications.</p><p>I am currently working on a few side projects such as an e-mail microservice and an API with an admin dashboard with CRUD capabilities.</p>',
+
+  data: ''
+}
